18bb99b0f25bb3e20326dca7c4b88991c8db706c
Ejperez3 <laloperez577@gmail.com>
2025-04-21 12:12:31 -0500

    Add files via upload
    Class day 2 updates to Sensor Condition

--- sensorCondition.sv
+++ sensorCondition.sv
@@ -1,3 +1,6 @@
+//// <CORRECT FILE>
+//// <CORRECT FILE>
+//// <CORRECT FILE>
 module sensorCondition(
     input logic clk, 
     input logic rst_n,
@@ -9,22 +12,72 @@ module sensorCondition(
     input logic [11:0] batt,
     output logic [12:0] error,
     output logic not_pedaling,
-    output logic TX        
+    output logic TX
 );
-
+        
 logic cadence_Sfilt; //filtered cadence signal  
-logic cadence_rise;  
-logic candance_per; //periof of filtered candance signal 
-logic not_pedaling;
+logic cadence_rise;  //
+logic candance_per;  //period of filtered candance signal 
+
+//used to detect falling edge on not_pedaling
+logic pedaling_resREG;
+logic pedaling_resumes; 
+
 logic [4:0] cadence; 
 
 //instantiate cadence filter module to filter raw candence signal 
-cadence_filt filt1((.clk(clk), .rst_n(rst_n) , .cadence(cadence_raw), .cadence_filt(cadence_Sfilt), .cadence_rise(cadence_rise));
-
+cadence_filt filt1(.clk(clk), .rst_n(rst_n) , .cadence(cadence_raw), .cadence_filt(cadence_Sfilt), .cadence_rise(cadence_rise));
 cadence_meas meas1( .clk(clk), .rst_n(rst_n), .cadence_filt(cadence_filt), .cadence_per(candance_per), .not_pedaling(not_pedaling));
+cadence_LU lu1(.cadence_per(candance_per),.cadence(cadence));
+
+//instantiate telemetry (also contains UART transmitter) ****NEEDS TO FILL IN PARAMS***
+telemetry telem1(.clk(clk), .rst_n(rst_n), .batt_v(batt), .avg_curr(curr_avg), .avg_torque(), .TX(TX));
+
+//instantiate desired drive to get target_curr ****NEEDS TO FILL IN PARAMS***
+desiredDrive drive1(.not_pedaling(not_pedaling), .avg_torque(), .cadence(cadence), .incline(), .scale(), .target_curr());
+
+//falling edge detector for not_pedaling
+always_ff @(posedge clk or negedge rst_n) begin 
+    if(!rst_n)
+        pedaling_resREG <= 0; 
+    else 
+        pedaling_resREG <=not_pedaling;    
+end 
+
+//falling edge on not_pedaling
+assign pedaling_resumes = ~not_pedaling & pedaling_resREG; 
+
+//<siganls used for current exponential average>
+logic [13:0] curr_accum; 
+logic [11:0] curr_avg;
+logic  include_smpl; 
+
+//determine when timer is full 
+generate if (FAST_SIM) 
+    logic [16:0] tmr_c;  //during fast simulation reduce bit width of counter 
+else
+    logic [22:0] tmr_c;  
+endgenerate
 
 
-cadence_LU(.cadence_per(candance_per),.cadence())
+//free running timer for current samples
+always_ff(@posedge clk or negedge rst_n)begin 
+    if(!rst_n)
+        tmr_c <= 0; 
+    else 
+        tmr_c <= tmr_c +1;     
+end 
+
+include_smpl = &tmr_c;
+
+//current exponential running average 
+always_ff @(posedge clk or negedge rst_n)begin 
+    if(!rst_n)
+        curr_accum <= 0;
+    else if(include_smpl)  
+      current_accum <=  ((curr_accum*3) >>> 2) + curr_avg;
+end 
+
 
 
 endmodule
@@ -84,24 +137,17 @@ wire cadence_rise;
 reg rise_reg;
 
 always(@posedge clk, negedge rst_n)begin
-    if(!rst_n)rise_reg<=1'b0;
-    else rise_reg<=cadence_filt;
+    if(!rst_n)
+        rise_reg<=1'b0;
+    else 
+        rise_reg<=cadence_filt;
 end
+
 assign cadence_rise=rise_reg&~cadence_filt;
 
 
 //on the rising edge of cadence_filt, a 24 bit timer is cleared,
-//BUT si
-
-endmodule
-    output wire[7:0] cadence_per_wire;
-    output wire not_pedaling;
-//24 bit counter
-reg[23:0] threeSecTimer;
-
-
-
-cadence_risecadence_rise) if FAST_SIM)
+//BUT its upper 8 bits are captured (or bits [14:7) if FAST_SIM)
 //are captured in an 8 bit register that forms cadence_per
 
 
@@ -124,29 +170,85 @@ scenario.
 //8 bit register that holes value of "cadence_per"
 //NOTE: CADENCE_PER HAS A SYNCHRONOUS RESET?!
 //STAGE 1 ASYNCH RESET. 
-wire[7:0] stage_1_output=(FAST_SIM)?threeSecTimer[14:7]:threeSecTimer[23:16];
+//should values out of fastsim?
+wire[7:0] stage_1_output = (FAST_SIM) ? threeSecTimer[14:7] : threeSecTimer[23:16];
 reg[7:0] cadence_per;
+wire capture_per;
 
-always(@posedge clk, negedge rst_n)begin
+assign capture_per= cadence_rise || (threeSecTimer == THIRD_SEC);
+
+always(@posedge clk, negedge rst_n) begin
     //check for syncrhonous reset?
-    if(!rst_n)begin
-        threeSecTimer<=24'b0;
+    if (!rst_n) begin
+        threeSecTimer <= 24'b0;
     end
+
     else begin
-        //if the rising edge is true, load either bits [23:16] if FAST_SIM is false, bits [14:7] if FAST_SIM is true. 
-        if(cadence_ise)begin
-            threeSecTimer<=24'b0;
-        end
-    
+        //if the rising edge is true, load either bits [23:16] if FAST_SIM is false,
+        //bits [14:7] if FAST_SIM is true. 
+
+        //if THIRD_SEC is true, then freeze the clock
+        if (cadence_rise) 
+            threeSecTimer <= 24'b0;
+
+        else if(threeSecTimer == THIRD_SEC) 
+            threeSecTimer <= threeSecTimer;
+
+        else 
+            threeSecTimer <= threeSecTimer + 1;
+        
+
+        //if the 24 bit timer gets to THIRD_SEC, then timer is frozen
+        //at that value and the value is captured in the 8-bit cadence_per registe
     end
-    
 end
 
 //STAGE 2 SYNCRHONOUS BLOCK
-always@(posedge clk)begin
-    
+wire[7:0] stg2Input;
+reg[7:0] cadence_per;
+wire not_pedaling;
 
+assign not_pedaling = (cadence_per == THIRD_SEC_UPPER);
+assign stg2Input = FAST_SIM ? (threeSecTimer[14:7]) : (threeSecTimer[23:16]);
 
+logic [7:0] cadence_per_input;
+always_comb begin
+    if(!rst_n) begin
+        cadence_per_input = THIRD_SEC_UPPER;
+    end else begin
+        if(capture_per) 
+            cadence_per_input=stg2Input;
+        else 
+            cadence_per_input=cadence_per;
+    end
 end
 
-7
+always@(posedge clk)begin
+    cadence_per <= cadence_per_input;
+end
+
+//TODO: implement sensorCondition for avg_torque. not as a module. 
+
+//KEY IDEA:
+/*
+sensors require averaging. (ie, avearge of last n samples). 
+normal averaging is expensive, perform exponential avrages. 
+exponential average gives more weight to more recent measurements than older measurements. 
+
+has an accumulator log2(w) bits wider than the bus width. So an exponential average of weight 16
+ie 16 past measurements(?), accumulator needs to be 4 bits wider than the bus width. 
+if bus width is 12 bits, then 16 bit accumulator. 
+
+for every new sample, accum is updated with
+accum=((accum*(w-1)/w))+smpl. 
+the average of the samples is then accum/w
+
+W should always be a power of 2 so its just bit shifting. 
+
+
+
+
+*/
+
+
+endmodule
