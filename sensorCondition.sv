e25437027e1a18ee4544c00e4fe631e5d9914dcf
Ejperez3 <laloperez577@gmail.com>
2025-04-23 12:30:39 -0500

    Add files via upload
    sensorCondition Complete (needs troubleshooting)

--- sensorCondition.sv
+++ sensorCondition.sv
@@ -6,15 +6,20 @@ module sensorCondition(
     input logic rst_n,
     input logic [11:0] torque,
     input logic cadence_raw,
-    input logic [11:0] curr,
+    input logic signed [11:0] curr,
     input logic [11:0] incline, 
     input logic [2:0] scale,
     input logic [11:0] batt,
-    output logic [12:0] error,
+    output logic signed [12:0] error,
     output logic not_pedaling,
     output logic TX
 );
         
+parameter FAST_SIM = 0;
+
+logic [11:0] curr_avg;
+logic [11:0] torque_avg;
+
 logic cadence_Sfilt; //filtered cadence signal  
 logic cadence_rise;  //
 logic candance_per;  //period of filtered candance signal 
@@ -25,16 +30,18 @@ logic pedaling_resumes;
 
 logic [4:0] cadence; 
 
+logic signed [11:0] target_curr;
+
 //instantiate cadence filter module to filter raw candence signal 
-cadence_filt filt1(.clk(clk), .rst_n(rst_n) , .cadence(cadence_raw), .cadence_filt(cadence_Sfilt), .cadence_rise(cadence_rise));
-cadence_meas meas1( .clk(clk), .rst_n(rst_n), .cadence_filt(cadence_filt), .cadence_per(candance_per), .not_pedaling(not_pedaling));
+cadence_filt #(.FAST_SIM(FAST_SIM)) filt1(.clk(clk), .rst_n(rst_n) , .cadence(cadence_raw), .cadence_filt(cadence_Sfilt), .cadence_rise(cadence_rise));
+cadence_meas #(.FAST_SIM(FAST_SIM)) meas1( .clk(clk), .rst_n(rst_n), .cadence_filt(cadence_filt), .cadence_per(candance_per), .not_pedaling(not_pedaling));
 cadence_LU lu1(.cadence_per(candance_per),.cadence(cadence));
 
 //instantiate telemetry (also contains UART transmitter) ****NEEDS TO FILL IN PARAMS***
-telemetry telem1(.clk(clk), .rst_n(rst_n), .batt_v(batt), .avg_curr(curr_avg), .avg_torque(), .TX(TX));
+telemetry telem1(.clk(clk), .rst_n(rst_n), .batt_v(batt), .avg_curr(curr_avg), .avg_torque(torque_avg), .TX(TX));
 
 //instantiate desired drive to get target_curr ****NEEDS TO FILL IN PARAMS***
-desiredDrive drive1(.not_pedaling(not_pedaling), .avg_torque(), .cadence(cadence), .incline(), .scale(), .target_curr());
+desiredDrive drive1(.not_pedaling(not_pedaling), .avg_torque(torque_avg), .cadence(cadence), .incline(incline), .scale(scale), .target_curr(target_curr));
 
 //falling edge detector for not_pedaling
 always_ff @(posedge clk or negedge rst_n) begin 
@@ -48,39 +55,66 @@ end
 assign pedaling_resumes = ~not_pedaling & pedaling_resREG; 
 
 //<siganls used for current exponential average>
-logic [13:0] curr_accum; 
-logic [11:0] curr_avg;
+logic [13:0] current_accum; 
+logic [13:0] newC_accum; 
+
 logic  include_smpl; 
+
+logic [22:0] tmr_c;  
 
 //determine when timer is full 
 generate if (FAST_SIM) 
-    logic [16:0] tmr_c;  //during fast simulation reduce bit width of counter 
+    assign include_smpl = &tmr_c[16:0];  //during fast simulation reduce bit width of counter 
 else
-    logic [22:0] tmr_c;  
+    assign include_smpl = &tmr_c;
 endgenerate
 
 
 //free running timer for current samples
-always_ff(@posedge clk or negedge rst_n)begin 
+always_ff @(posedge clk or negedge rst_n)begin 
     if(!rst_n)
         tmr_c <= 0; 
     else 
         tmr_c <= tmr_c +1;     
 end 
 
-include_smpl = &tmr_c;
 
 //current exponential running average 
 always_ff @(posedge clk or negedge rst_n)begin 
     if(!rst_n)
-        curr_accum <= 0;
+       current_accum <= 0;
     else if(include_smpl)  
-      current_accum <=  ((curr_accum*3) >>> 2) + curr_avg;
+      current_accum <= newC_accum;
 end 
 
+assign newC_accum = ((current_accum * 3) >>> 2) + curr;  //formula for exponential running average 
+assign curr_avg = current_accum[13:2]; //average is current accum / 4
+
+//<siganls used for torque exponential average>
+logic [16:0] torque_accum; 
+logic [16:0] newT_accum;
+
+
+//current exponential running average 
+always_ff @(posedge clk or negedge rst_n) begin 
+    if(!rst_n)
+        torque_accum <= 0;
+    else if(pedaling_resumes)  
+        torque_accum <= {1'b0,torque, 4'b0000};
+    else if(cadence_rise)
+        torque_accum <= newT_accum;
+end
+
+//formula for torque running average 
+assign newT_accum = ((torque_accum * 31) / 32) + torque;
+assign torque_avg = torque_accum[16:5];
+
+
+assign error = target_curr - curr_avg;
 
 
 endmodule
+
 
 
 module cadence_meas(
@@ -96,9 +130,21 @@ localparam THIRD_SEC_REAL = 24'hE4E1C0;
 localparam THIRD_SEC_FAST = 24'h007271;
 localparam THIRD_SEC_UPPER = 8'hE4;
 
+parameter FAST_SIM = 0;
+logic cadence_rise;
+logic [23:0] THIRD_SEC;
+
+generate if (FAST_SIM)
+   assign THIRD_SEC = THIRD_SEC_FAST;
+else
+   assign THIRD_SEC = THIRD_SEC_REAL;
+endgenerate
+
 logic capture_per;
 logic third_sec_equals;
 logic [23:0] third_sec_cnt;
+
+
 
 //equals block for THIRD_SEC
 always_comb begin
@@ -126,24 +172,18 @@ always_ff @(posedge clk or negedge rst_n) begin
 end
     
 
-generate if (FAST_SIM)
-    localparam THIRD_SEC = THIRD_SEC_FAST;
-else
-    localparam THIRD_SEC = THIRD_SEC_REAL;
-endgenerate
-
 // rising edge detection for cadence filt
-wire cadence_rise;
-reg rise_reg;
 
-always(@posedge clk, negedge rst_n)begin
+logic rise_reg;
+
+always@(posedge clk, negedge rst_n)begin
     if(!rst_n)
         rise_reg<=1'b0;
     else 
         rise_reg<=cadence_filt;
 end
 
-assign cadence_rise=rise_reg&~cadence_filt;
+assign cadence_rise = rise_reg &~ cadence_filt;
 
 
 //on the rising edge of cadence_filt, a 24 bit timer is cleared,
@@ -171,13 +211,12 @@ scenario.
 //NOTE: CADENCE_PER HAS A SYNCHRONOUS RESET?!
 //STAGE 1 ASYNCH RESET. 
 //should values out of fastsim?
-wire[7:0] stage_1_output = (FAST_SIM) ? threeSecTimer[14:7] : threeSecTimer[23:16];
-reg[7:0] cadence_per;
-wire capture_per;
+logic [23:0] threeSecTimer;
 
-assign capture_per= cadence_rise || (threeSecTimer == THIRD_SEC);
+logic [7:0] stage_1_output = (FAST_SIM) ? threeSecTimer[14:7] : threeSecTimer[23:16];
 
-always(@posedge clk, negedge rst_n) begin
+
+always@(posedge clk, negedge rst_n) begin
     //check for syncrhonous reset?
     if (!rst_n) begin
         threeSecTimer <= 24'b0;
@@ -197,16 +236,13 @@ always(@posedge clk, negedge rst_n) begin
         else 
             threeSecTimer <= threeSecTimer + 1;
         
-
         //if the 24 bit timer gets to THIRD_SEC, then timer is frozen
         //at that value and the value is captured in the 8-bit cadence_per registe
     end
 end
 
 //STAGE 2 SYNCRHONOUS BLOCK
-wire[7:0] stg2Input;
-reg[7:0] cadence_per;
-wire not_pedaling;
+logic [7:0] stg2Input;
 
 assign not_pedaling = (cadence_per == THIRD_SEC_UPPER);
 assign stg2Input = FAST_SIM ? (threeSecTimer[14:7]) : (threeSecTimer[23:16]);
@@ -226,29 +262,6 @@ end
 always@(posedge clk)begin
     cadence_per <= cadence_per_input;
 end
-
-//TODO: implement sensorCondition for avg_torque. not as a module. 
-
-//KEY IDEA:
-/*
-sensors require averaging. (ie, avearge of last n samples). 
-normal averaging is expensive, perform exponential avrages. 
-exponential average gives more weight to more recent measurements than older measurements. 
-
-has an accumulator log2(w) bits wider than the bus width. So an exponential average of weight 16
-ie 16 past measurements(?), accumulator needs to be 4 bits wider than the bus width. 
-if bus width is 12 bits, then 16 bit accumulator. 
-
-for every new sample, accum is updated with
-accum=((accum*(w-1)/w))+smpl. 
-the average of the samples is then accum/w
-
-W should always be a power of 2 so its just bit shifting. 
-
-
-
-
-*/
 
 
 endmodule
